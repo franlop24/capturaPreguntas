61f0757ac926ca40414c537f9663dfa190703cd1
Francisco Lopez <franlopbri@gmail.com>
2020-07-03 21:08:08 -0500

    crea funciones de Consulta y evaluaci´

--- public/js/config/ConfigFirebase.js
+++ public/js/config/ConfigFirebase.js
@@ -1,9 +1,21 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCz-oWoH6mmpruqd5rfllOGE86xAJGnYWY",
-    authDomain: "seaniutt.firebaseapp.com",
-    databaseURL: "https://seaniutt.firebaseio.com",
-    projectId: "seaniutt",
-    storageBucket: "seaniutt.appspot.com",
-    messagingSenderId: "1050232849332",
-    appId: "1:1050232849332:web:de1025c4aaac7a3526a8e1"
-};
+    apiKey: "AIzaSyAyiLjNMCdX4vkB_kM0n8D_KE-tWhzGGg4",
+    authDomain: "seani-954ac.firebaseapp.com",
+    databaseURL: "https://seani-954ac.firebaseio.com",
+    projectId: "seani-954ac",
+    storageBucket: "seani-954ac.appspot.com",
+    messagingSenderId: "821462862987",
+    appId: "1:821462862987:web:43310b4bbc8b84f354d88e",
+    measurementId: "G-EXBFMRJ8F4"
+  };
+
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCz-oWoH6mmpruqd5rfllOGE86xAJGnYWY",
+//     authDomain: "seaniutt.firebaseapp.com",
+//     databaseURL: "https://seaniutt.firebaseio.com",
+//     projectId: "seaniutt",
+//     storageBucket: "seaniutt.appspot.com",
+//     messagingSenderId: "1050232849332",
+//     appId: "1:1050232849332:web:de1025c4aaac7a3526a8e1"
+// };
--- public/js/preguntas/preguntas.js
+++ public/js/preguntas/preguntas.js
@@ -4,9 +4,9 @@ class Pregunta {
         //const settings = { timestampsInSnapshots : true };
         //this.db.settings(settings);
     }
-  
+
     crearPregunta (id_pregunta, preguntatxt, preguntaurl, respuesta1, respuesta2, respuesta3, respuesta4, correcta) {
-  
+
         return this.db.collection('preguntas').add({
             id_pregunta : id_pregunta,
             preguntatxt : preguntatxt,
@@ -35,16 +35,147 @@ class Pregunta {
     }
 
     consultarTodasPreguntas () {
-      this.db.collection('preguntas')
-          .onSnapshot(querySnapshot => {
-              console.log(querySnapshot);
-      })
+        this.db.collection('preguntas')
+        .onSnapshot(querySnapshot => {
+            let preguntasExam = [];
+            querySnapshot.forEach(pregunta => {
+                let preguntaActual = {
+                    id_pregunta : pregunta.data().id_pregunta,
+                    preguntatxt : pregunta.data().preguntatxt,
+                    preguntaurl : pregunta.data().preguntaurl,
+                    respuesta1 : pregunta.data().respuesta1,
+                    respuesta2 : pregunta.data().respuesta2,
+                    respuesta3 : pregunta.data().respuesta3,
+                    respuesta4 : pregunta.data().respuesta4
+                }
+                preguntasExam.push(preguntaActual);
+                //console.log(preguntaActual);
+            })
+            console.log(preguntasExam);
+            localStorage.setItem('preguntas', JSON.parse(preguntasExam));
+        })
     }
-  
+
+    cuentaTerminados(){
+        this.obtieneCorrectas()
+        this.db.collection('alumnos')
+        .where("activeExam1", "==", false)
+        .onSnapshot(querySnapshot => {
+            let preguntasExam = [];
+            querySnapshot.forEach(alumno => {
+                let preguntaActual = {
+                    username : alumno.data().username,
+                    //user: alumno.data().user,
+                    respuestas: alumno.data().alumnExam,
+                    carrera: alumno.data().alumnData.carrera
+                }
+                preguntasExam.push(preguntaActual);
+                //calificaExamen(preguntaActual.user);
+                //console.log(preguntaActual);
+                preguntaActual.resultado = this.obtieneResultados(preguntaActual.respuestas);
+                console.log(preguntaActual);
+            })
+            console.log(preguntasExam.length);
+        })
+    }
+
+    obtieneResultados(dataAlumno){
+        let cuentaCorrectas = 0;
+        dataAlumno.test.forEach(respuesta => {
+            let buscaenCorrectas = this.CorrectasExam.find(element => element.pregunta == respuesta.pregunta);
+            if(buscaenCorrectas){
+                if(buscaenCorrectas.correcta == respuesta.respuesta){
+                    cuentaCorrectas++;
+                }
+            }
+        })
+        //console.log(cuentaCorrectas);
+        return cuentaCorrectas;
+    }
+
+    cuentaPorTerminar(){
+      this.db.collection('alumnos')
+        .where("time", "==", 7200)
+        .onSnapshot(querySnapshot => {
+            let preguntasExam = [];
+            querySnapshot.forEach(alumno => {
+                let preguntaActual = {
+                    username : alumno.data().username,
+                    carrera: alumno.data().alumnData.carrera
+                }
+                preguntasExam.push(preguntaActual);
+                //console.log(preguntaActual);
+            })
+            console.log(preguntasExam);
+            console.log(preguntasExam.length);
+        })
+    }
+
+    calificaExamen(userId){
+        this.obtieneCorrectas();
+        //let Calificar;
+        this.db.collection('alumnos')
+            .where("user","==", userId)
+            .onSnapshot(querySnapshot => {
+                let alumnoData = []
+                querySnapshot.forEach(alumno => {
+                    let alumnoActual = {
+                        username: alumno.data().username,
+                        carrera: alumno.data().alumnData.carrera,
+                        respuestas: alumno.data().alumnExam
+                    }
+                    alumnoData.push(alumnoActual)
+                })
+                console.log(alumnoData[0]);
+                //Calificar = alumnoData[0];
+            
+            let cuentaCorrectas = 0;
+            alumnoData[0].respuestas.test.forEach(respuesta => {
+                let buscaenCorrectas = this.CorrectasExam.find(element => element.pregunta == respuesta.pregunta);
+                if(buscaenCorrectas){
+                    if(buscaenCorrectas.correcta == respuesta.respuesta){
+                        cuentaCorrectas++;
+                    }
+                }
+            })
+            console.log(cuentaCorrectas);
+        })
+    }
+
+    obtieneCorrectas(){
+        this.db.collection('correctas')
+            .onSnapshot(querySnapshot => {
+                let correctas = []
+                querySnapshot.forEach(correcta => {
+                    let correctaActual = {
+                        pregunta: correcta.data().id_pregunta,
+                        correcta: correcta.data().correcta
+                    }
+                    correctas.push(correctaActual);
+                })
+                this.CorrectasExam = correctas;
+            })
+    }
+
+    cuentaTodos(){
+      this.db.collection('alumnos')
+        .onSnapshot(querySnapshot => {
+            let preguntasExam = [];
+            querySnapshot.forEach(alumno => {
+                let preguntaActual = {
+                    username : alumno.data().username
+                }
+                preguntasExam.push(preguntaActual);
+                //console.log(preguntaActual);
+            })
+            console.log(preguntasExam.length);
+        })
+    }
+
     subirImagenPregunta(file){
         const refStorage = firebase.storage().ref(`imgPreguntas/${file.name}`);
         const task = refStorage.put(file);
-  
+
         task.on('state_changed',
           snapshot => {
               const porcentaje = snapshot.bytesTransferred / snapshot.totalBytes * 100;
@@ -60,12 +191,11 @@ class Pregunta {
                       console.log(url);
                       sessionStorage.setItem('imgNewPreg', url);
                   })
-                  .catch( error => { 
+                  .catch( error => {
                       console.log(`Error obteniendo downloadURL => ${error}`, 4000);
                   })
           }
           )
     }
-  
+
   }
-  
